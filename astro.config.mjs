2a65ea6ef7d85d4686fe7aa1720d60928f32d2a9
Zachary Stout <stout.zachary@gmail.com>
2023-09-19 18:19:00 -0600

    please deploy?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/staic";
+import vercel from "@astrojs/vercel/static";
+
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "sever",
+  output: "static",
   adapter: vercel()
 });
