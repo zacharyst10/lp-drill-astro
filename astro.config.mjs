e7f359699741226f42544e70c3ba9db82d5566a0
Zachary Stout <stout.zachary@gmail.com>
2023-10-08 22:17:26 -0600

    prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vercelStatic from '@astrojs/vercel/static';
 
+import prefetch from "@astrojs/prefetch";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), prefetch()],
   output: 'static',
-  adapter: vercelStatic(),
+  adapter: vercelStatic()
 });
