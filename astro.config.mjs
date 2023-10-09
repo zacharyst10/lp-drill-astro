60b60623696941d90693e394478c6d9a14a3716b
Zachary Stout <stout.zachary@gmail.com>
2023-10-09 12:46:22 -0600

    back to the good old image tag...

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel/serverless';
+import vercelStatic from '@astrojs/vercel/static';
+
 
 import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), prefetch()],
-  output: 'server',
-  adapter: vercel(),
+  output: 'static',
+  adapter: vercelStatic()
 });
