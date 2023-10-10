b43cb4648c2182baf1ddc00ea532b363dff0da1e
Zachary Stout <stout.zachary@gmail.com>
2023-10-09 22:23:29 -0600

    SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import vercelStatic from '@astrojs/vercel/static';
+import vercel from '@astrojs/vercel/serverless';
 
 
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), ],
-  output: 'static',
-  adapter: vercelStatic()
+  output: 'server',
+  adapter: vercel()
 });
