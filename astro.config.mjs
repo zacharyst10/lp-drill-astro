a019ed4e9ae003fb9b28e07aa1b4f005028af1be
Zachary Stout <stout.zachary@gmail.com>
2023-12-05 14:37:56 -0700

    pagefind is out again

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vercel from '@astrojs/vercel/static';
-import pagefind from 'astro-pagefind';
 
 
 
 // https://astro.build/config
 export default defineConfig({
-  build: {
-    format: "file",
-  },
-  integrations: [tailwind(), pagefind(), ],
+ 
+  integrations: [tailwind(),  ],
   output: 'static',
   adapter: vercel({
     webAnalytics: {
