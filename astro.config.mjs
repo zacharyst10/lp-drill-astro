2c39192b25929c41acee0fcd416f4e890e1bec7a
zacharyst10 <104918230+zacharyst10@users.noreply.github.com>
2025-02-03 10:53:39 -0700

    updates from Kristi (#12)
    * updates from kristi
    
    * additional edits from Kristi
    
    * forgot one
    
    * typo

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
- 
+  
   integrations: [tailwind(),  ],
   output: 'static',
   adapter: vercel({
